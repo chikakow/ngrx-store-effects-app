5291779b4cbdb3f970f24913471d41d8f0b7ed91
Chikako Wakishima <ChikakoW@sedc.sedata.com>
2018-06-19 17:21:21 -0400

    router-store not firing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {
+  StoreRouterConnectingModule,
+  RouterStateSerializer
+} from '@ngrx/router-store';
+
 import { StoreModule, MetaReducer } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
-import { reducers } from './store'; 
+import { reducers, CustomSerializer } from './store'; 
 
 // not used in production
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -43,6 +48,7 @@ export const ROUTES: Routes = [
     EffectsModule.forRoot([]),
     environment.development ? StoreDevtoolsModule.instrument() : [],
   ],
+  providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
